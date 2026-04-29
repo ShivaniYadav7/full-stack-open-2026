fe9e8ff853347143c67977589cf96cb45d394acd
ShivaniYadav7 <shivaniyadav84237@gmail.com>
2026-04-29 11:53:06 +0530

    Finish exercises 1.3 - 1.5: refactor to single course object

--- part1/src/components/Content.jsx
+++ part1/src/components/Content.jsx
@@ -3,9 +3,9 @@ import Part from './Part'
 const Content = (props) => {
     return (
     <>
-        <p>Part name = {props.part1} exercises = {props.ex1}</p>
-        <p>Part name = {props.part2} exercises = {props.ex2}</p>
-        <p>Part name = {props.part3} exercises = {props.ex3}</p>
+        <Part name = {props.parts[0].name} exercises = {props.parts[0].exercises} />
+        <Part name = {props.parts[1].name} exercises = {props.parts[1].exercises} />
+        <Part name = {props.parts[2].name} exercises = {props.parts[2].exercises} />
     </>
     )
 }
--- part1/src/components/Total.jsx
+++ part1/src/components/Total.jsx
@@ -1,6 +1,7 @@
 const Total = (props) => {
+    const totalAmount = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises;
     return (
-    <p>This course has total {props.total} exercises</p>
+    <p>This course has total {totalAmount} exercises</p>
     )
 }
 
