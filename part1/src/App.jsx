c16ad9c79c71d3a0b9c0f07f1960e52f2305574f
ShivaniYadav7 <shivaniyadav84237@gmail.com>
2026-05-01 23:06:08 +0530

    finish ex 1.9

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -11,8 +11,21 @@ const Statistics = (props) => {
       const neutral = props.neutral
       const bad = props.bad
       const all = good + neutral + bad
+
+      if(all === 0) {
+        return (
+          <>
+          <h2>Statistics</h2>
+          <p>No feedback given</p>
+          </>
+        );
+      }
+      
       const average = all === 0? 0 : (good - bad) / all
       const positive = all === 0? 0 : (good / all) * 100
+
+      
+
   return(
     <>      
       <h2>statistics</h2>
@@ -32,17 +45,17 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + bad + neutral
 
-  return (
-    <div>
+      return (
+      <>
       <h2>give feedback</h2>
       <Button onClick={() => setGood(good + 1)} text='good'/>
       <Button onClick={() => setNeutral(neutral + 1)} text = 'neutral'/>
       <Button onClick={() => setBad(bad + 1)} text = 'bad'/>
-
       <Statistics good = {good} neutral = {neutral} bad = {bad}/>
-    </div>
-  )
+      </>
+    );
 }
 
 export default App
