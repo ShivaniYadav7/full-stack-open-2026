d5d685e21d78fafbeb5d8e22baa8aede80db28a7
ShivaniYadav7 <shivaniyadav84237@gmail.com>
2026-05-01 22:56:36 +0530

    finish ex 1.8

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -5,6 +5,26 @@ const Button = (props) => {
   <button onClick={props.onClick}>{props.text}</button>
   )
 }
+
+const Statistics = (props) => {
+  const good = props.good
+      const neutral = props.neutral
+      const bad = props.bad
+      const all = good + neutral + bad
+      const average = all === 0? 0 : (good - bad) / all
+      const positive = all === 0? 0 : (good / all) * 100
+  return(
+    <>      
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad  {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </>
+  );
+}
   
 
 const App = () => {
@@ -13,10 +33,6 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const all = good + neutral + bad
-  const average = all === 0 ? 0 : (good - bad) / all
-  const positive = all === 0 ? 0 : (good / all) * 100
-
   return (
     <div>
       <h2>give feedback</h2>
@@ -24,13 +40,7 @@ const App = () => {
       <Button onClick={() => setNeutral(neutral + 1)} text = 'neutral'/>
       <Button onClick={() => setBad(bad + 1)} text = 'bad'/>
 
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad  {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+      <Statistics good = {good} neutral = {neutral} bad = {bad}/>
     </div>
   )
 }
