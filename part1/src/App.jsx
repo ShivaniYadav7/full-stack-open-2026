5c048ee0e5d2a4ce6f7dd7614fb5198f7b59913e
ShivaniYadav7 <shivaniyadav84237@gmail.com>
2026-05-01 22:27:51 +0530

    finish ex 1.6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,35 +1,29 @@
-import Content from './components/Content'
-import Header from './components/Header'
-import Total from './components/Total'
-import Part from './components/Part'
+import { useState } from 'react'
+
+const Button = (props) => {
+  return (
+  <button onClick={props.onClick}>{props.text}</button>
+  )
+}
+  
 
 const App = () => {
-  const course = {
-  name: 'Half Stack application development',
-  parts: [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-}
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <div>
-        <Header course={course.name} />
-        <Content parts={course.parts} />
-        <Total parts={course.parts} />
-      </div>
-      
+      <h2>give feedback</h2>
+      <Button onClick={() => setGood(good + 1)} text='good'/>
+      <Button onClick={() => setNeutral(neutral + 1)} text = 'neutral'/>
+      <Button onClick={() => setBad(bad + 1)} text = 'bad'/>
+
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad  {bad}</p>
     </div>
   )
 }
