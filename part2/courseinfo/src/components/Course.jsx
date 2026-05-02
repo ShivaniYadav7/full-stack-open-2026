87aeaa17a5b5aed9febb179988ad8962d11802fd
ShivaniYadav7 <shivaniyadav84237@gmail.com>
2026-05-02 18:19:44 +0530

    complete ex 2.3 to ex 2.5

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,11 +1,15 @@
 import Header from './Header'
 import Content from './Content'
 
-const Course = ({ course }) => {
+const Course = ({ courses }) => {
     return (
-        <>        
+        <>
+        {courses.map(course => 
+        <div key = {course.id}>
         <Header name = {course.name}/>
         <Content parts = {course.parts}/>
+        </div>
+        )}
         </>
     )
 }
