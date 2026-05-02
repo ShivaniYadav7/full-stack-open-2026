9cf64233357ae7877e23d9bb254d341b9469aaeb
ShivaniYadav7 <shivaniyadav84237@gmail.com>
2026-05-02 17:46:45 +0530

    complete ex 2.1

---
+++ part2/courseinfo/src/components/Content.jsx
@@ -0,0 +1,13 @@
+import Part from './Part'
+
+const Content = (props) => {
+    return (
+        <>        
+        {props.parts.map(part => 
+            <Part key = {part.id} name = {part.name} exercises = {part.exercises}/>
+        )}
+        </>
+    )
+}
+
+export default Content
