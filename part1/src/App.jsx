7666376b2b6629fd14b9d790ac9e85782cb7969d
ShivaniYadav7 <shivaniyadav84237@gmail.com>
2026-05-02 10:39:21 +0530

    finish ex 1.13

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -13,11 +13,24 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+  const handleNext = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomIndex)
+  }
+
+  const handleVote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
 
   return (
     <div>
       <p>{anecdotes[selected]}</p>
-      <button onClick={() => setSelected(Math.floor(anecdotes.length * Math.random()))}>next anecdote</button>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={handleVote}>vote</button>
+      <button onClick={handleNext}>next anecdote</button>      
     </div>
   )
 }
